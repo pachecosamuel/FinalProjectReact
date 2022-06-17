490d3e18bd08c6cc3208abfd6d6e45a5bb199ca1
Bernardo Goncalves Rocha <bernardo.grocha@yahoo.com.br>
2022-06-17 17:26:24 -0300

    ideia de cards na home

--- reactecommerce/src/components/routes/root.js
+++ reactecommerce/src/components/routes/root.js
@@ -3,7 +3,7 @@ import React from "react";
 import { NavBar } from "../Layout/NavBar"
 import { Footer } from "../Layout/Footer"
 
-import { Home } from '../pages/Home'
+import  Home  from '../pages/Home'
 import { Sobre } from '../pages/About'
 import { NotFound } from '../pages/NotFound'
 import { ProdutoApi } from '../pages/Products'
