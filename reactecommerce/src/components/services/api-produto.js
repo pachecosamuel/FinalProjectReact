ffd1f885fa0557321d9b4eaf50c8d2bde1226a15
mattpsvreis <mateuspsvreis@gmail.com>
2022-06-17 21:47:48 -0300

    Consertando api-produto

--- reactecommerce/src/components/services/api-produto.js
+++ reactecommerce/src/components/services/api-produto.js
@@ -1,13 +1,13 @@
-import API from "./Api";
+import { api } from "./Api";
 
 export function ObterProdutos() {
   return new Promise((resolve, reject) => {
-    return API.get("/produto").then(res => resolve(res)).catch(err => reject(err));
+    return api.get("/produto").then(res => resolve(res)).catch(err => reject(err));
   });
 }
 
 export function ObterProduto(id) {
     return new Promise((resolve, reject) => {
-        return API.get(`/produto/${id}`).then(res => resolve(res)).catch(err => reject(err));
+        return api.get(`/produto/${id}`).then(res => resolve(res)).catch(err => reject(err));
       });
 }
