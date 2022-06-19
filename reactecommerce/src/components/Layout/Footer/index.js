235428aa518daed223d9b2d05e5b00fa06d169a1
JessySabino <bornjs17@gmail.com>
2022-06-19 13:50:33 -0300

    Atualizando Footer

--- reactecommerce/src/components/Layout/Footer/index.js
+++ reactecommerce/src/components/Layout/Footer/index.js
@@ -1,11 +1,42 @@
 import React from "react";
-import { Caixa, Teste } from "./style";
+import { Caixa, Container, Column, Texto, Paragrafo, Cabecalho } from "./style";
+
 
 export const Footer = () => {
 
     return (
         <Caixa>
-            <Teste>Eu sou o footer</Teste>
+            <Container>
+        <Texto>
+          <Column>
+            <Cabecalho>Contato</Cabecalho>
+            <Paragrafo>(24)2222-0000</Paragrafo>
+            <Paragrafo>saturnogeek@gmail.com</Paragrafo>
+          </Column>
+          <Column>
+            <Cabecalho>Atendimento ao cliente</Cabecalho>
+            <Paragrafo>Fale conosco</Paragrafo>
+            <Paragrafo>Perguntas frequentes</Paragrafo>
+          </Column>
+          <Column>
+            <Cabecalho>Redes Sociais</Cabecalho>
+            <Paragrafo href='#'>
+              <i className='fab fa-facebook-f'>
+                <span style={{ marginLeft: '10px' }}>Facebook</span>
+              </i>
+            </Paragrafo>
+            <Paragrafo href='#'>
+              <i className='fab fa-instagram'>
+                <span style={{ marginLeft: '10px' }}>Instagram</span>
+              </i>
+            </Paragrafo>
+          </Column>
+          <Column>
+            <Cabecalho>Horário de funcionamento</Cabecalho>
+            <Paragrafo>Todos os dias 00:00 - 00:00</Paragrafo>
+          </Column>
+        </Texto>
+      </Container>
         </Caixa>
     );
 }
--- reactecommerce/src/components/Layout/Footer/style.js
+++ reactecommerce/src/components/Layout/Footer/style.js
@@ -2,16 +2,47 @@ import React from "react";
 import styled from "styled-components";
 
 export const Caixa = styled.div `
-    display: flex;
-    justify-content: center;
+  padding: 10px 10px;
+  background: #192231;
+  width: 100%;
+`
 
-    background-color: #192231;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1000px;
+  margin: 0 auto;
+`
 
-    border: 4px solid tomato;
-    border-radius: 10px;
-`;
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 50px;
+`
 
-export const Teste = styled.h1 `
-    font-size: 1.5rem;
-    color: #fff;
-`;
+
+export const Texto = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
+  grid-gap: 10px;
+`
+
+export const Paragrafo = styled.a`
+  color: aliceblue;
+  margin-bottom: 5px;
+  font-size: 12px;
+  text-decoration: none;
+  &:hover {
+    color: #985e6d;
+    transform: scale(110%);
+  }
+`
+
+export const Cabecalho = styled.p`
+  font-size: 18px;
+  color: aliceblue;
+  margin-bottom: 10px;
+  font-weight: bold;
+`
