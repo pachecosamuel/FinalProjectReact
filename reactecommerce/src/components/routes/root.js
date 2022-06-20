65b1628a8c13f7e6b51b32e1657bd22679655337
mattpsvreis <mateuspsvreis@gmail.com>
2022-06-20 18:36:47 -0300

    Adicionando pages 'Admin' e 'Carrinho'

--- reactecommerce/src/components/routes/root.js
+++ reactecommerce/src/components/routes/root.js
@@ -6,9 +6,11 @@ import { Footer } from "../Layout/Footer"
 import { Home } from '../pages/home'
 import { Sobre } from '../pages/about'
 import { SobreDevs } from '../pages/aboutdevs'
-import { NotFound } from '../pages/NotFound'
+import { NotFound } from '../pages/notfound'
 import { ProdutoApi } from '../pages/products'
-import { Product } from '../pages/Product'
+import { Product } from '../pages/product'
+import { Cart } from '../pages/cart'
+
 import {
     BrowserRouter as Router,
     Routes,
@@ -27,6 +29,7 @@ export const Root = () => {
                 <Route path="/sobre" element={<Sobre />} />
                 <Route path="/sobre/devs" element={<SobreDevs />} />
                 <Route path="/produto/:idUseParams" element={<Product />} />
+                <Route path="/carrinho" element={<Cart />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
