c46f11daaa76535265702a27266e468409acde8e
Samuel Pacheco <pacheco.samuel@hotmail.com>
2022-06-20 20:02:04 -0300

    Font size footer

--- reactecommerce/src/components/Layout/Footer/style.js
+++ reactecommerce/src/components/Layout/Footer/style.js
@@ -31,7 +31,7 @@ export const Texto = styled.div`
 export const Paragrafo = styled.a`
   color: aliceblue;
   margin-bottom: 5px;
-  font-size: 1rem;
+  font-size: 1.1rem;
   text-decoration: none;
   cursor: pointer;
   transition: 0.2s;
