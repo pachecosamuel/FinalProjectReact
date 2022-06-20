40d5691d923f8c2216450d5f554cf51da56d39a2
mattpsvreis <mateuspsvreis@gmail.com>
2022-06-20 17:40:19 -0300

    Ajustes de cor

--- reactecommerce/src/components/pages/about/style.js
+++ reactecommerce/src/components/pages/about/style.js
@@ -4,23 +4,12 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     padding-top: 30px;
-    /* align-items: center;
-    justify-content: center; */
-
     width: 85%;
-    /* height: 70vh; */
-
     margin: 1rem auto 1rem;
-
-    background-color: #192231;
-    /* border: 5px solid black; */
 `;
 
 export const Organizacao = styled.div`
     display: flex;
-    /* background-color: darksalmon; */
-    /* border: 5px solid goldenrod;
-    border-radius: 280px; */
 `;
 
 export const Texto = styled.h1`
